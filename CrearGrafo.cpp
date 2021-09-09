475c3a9654fcde87e7cfc21d4f3b902ad84bcfaf
agustinsaa <90406819+agustinsaa@users.noreply.github.com>
2021-09-09 12:54:09 -0300

    Create CrearGrafo.cpp

---
+++ CrearGrafo.cpp
@@ -0,0 +1,31 @@
+// Crear un grafo en C++
+
+#include <iostream>
+#include <vector>
+
+int main(){
+    int n; // n es el número de vértices
+    int m; // m es el número de aristas 
+
+    cin >> n >> m   // Lee por entrada el número de vértices y aristas del grafo G
+
+    // G es el grafo que se va a crear a través de la matriz de adyacencia
+
+    vector<vector<int>> G(n);
+
+    for(int i=0; i < m; ++i){
+        int a, b;
+        cin >> a >> b;  // Se lee una arista (a,b)
+
+        // Se introduce los vértices a y b
+        G[a].push_back(b);
+        G[b].push_back(a);
+
+    }
+
+    // Se crea el grafo y se imprime finalmente 
+    for(int i=0; i < n; ++i){
+        cout << "Los vertices adyacentes a " << i << ": ";
+        for(int j=0; j < G[i].size(); ++j) cout << G[i][j];
+        cout << endl; 
+    }
